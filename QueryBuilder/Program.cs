80e6b60b16246296e3a4283e056aef55cb51de65
Kristian Montoya <kristian.s.montoya@gmail.com>
2025-07-04 13:36:05 -0500

    ratings sql

---
+++ QueryBuilder/Program.cs
@@ -0,0 +1,63 @@
+﻿using IMDB_DB;
+
+string inputPath = @$"D:\IMDB_SQL\data\{RatingFileSchema.FileName}";
+
+int batchSize = 1000; // Number of rows per INSERT statement
+
+using var reader = new StreamReader( inputPath );
+
+var valueBatch = new RatingDto[batchSize];
+string? line;
+
+int readerLine = 0;
+int sqlLineCount = 0;
+int currentBatchCount = 0;
+while( ( line = reader.ReadLine() ) != null ) {
+    if( readerLine == 0 ) {
+        readerLine++;
+        continue;
+    }
+
+    valueBatch[sqlLineCount] = new RatingDto( line, '\t' );
+    sqlLineCount++;
+
+    if( sqlLineCount >= batchSize ) {
+        WriteInsert( valueBatch, currentBatchCount );
+        currentBatchCount++;
+        sqlLineCount = 0;
+        valueBatch = new RatingDto[batchSize];
+    }
+
+    readerLine++;
+}
+
+if( valueBatch.Count() > 0 ) {
+    WriteInsert( valueBatch, currentBatchCount );
+}
+
+Console.WriteLine( "Done." );
+
+static void WriteInsert( RatingDto[] values, int currentBatchCount )
+{
+    Directory.CreateDirectory( @"D:\IMDB_SQL\output" );
+
+    string fileName = $"rating_sql{( currentBatchCount == 0 ? string.Empty : $"_{currentBatchCount}" )}.sql";
+    string outputPath = @$"D:\IMDB_SQL\output\{fileName}";
+    Console.WriteLine( $"Creating file, {fileName}..." );
+
+    using var writer = new StreamWriter( outputPath, append: false ); // Overwrites existing file
+
+    string insertHeader = $"INSERT INTO {RatingFileSchema.SqlTableName} ({RatingFileSchema.ImdbIdColName}, {RatingFileSchema.RatingColName}, {RatingFileSchema.NumVotesColName}) VALUES";
+
+    int currentIndex = 0;
+    int lastIndex = values.Where( v => v != null ).Count() - 1;
+    writer.WriteLine( insertHeader );
+    foreach( var lineValues in values ) {
+        if( lineValues == null ) {
+            break;
+        }
+        string dataInsert = $"('{lineValues.ImdbId}','{lineValues.Rating}','{lineValues.NumVotes}'){( currentIndex != lastIndex ? "," : string.Empty )}";
+        writer.WriteLine( dataInsert );
+        currentIndex++;
+    }
+}
---
+++ QueryBuilder/RatingDto.cs
@@ -0,0 +1,23 @@
+﻿namespace IMDB_DB
+{
+    public class RatingDto
+    {
+        public RatingDto( string dataLine, char delimiter )
+        {
+            string[] t = dataLine.Split( delimiter );
+
+            ImdbId = t[RatingFileSchema.ImdbIdIndex];
+            if(decimal.TryParse(t[RatingFileSchema.RatingIndex], out decimal rating ) ) {
+                Rating = rating;
+            }
+
+            if(int.TryParse(t[RatingFileSchema.NumVotesIndex], out int numVotes ) ) {
+                NumVotes = numVotes;
+            }
+        }
+
+        public string ImdbId { get; set; }
+        public decimal Rating { get; set; } = 0;
+        public int NumVotes { get; set; } = 0;
+    }
+}
