cd83ccbc936c39bcd246ab7d05276e63efb5d121
Kristian Montoya <kristian.s.montoya@gmail.com>
2025-07-05 16:34:34 -0500

    check in

--- config.inc.php
+++ config.inc.php
@@ -2,9 +2,9 @@
 $i = 0;
 $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
-$cfg['Servers'][$i]['host'] = 'PeardeVault_DB';
+$cfg['Servers'][$i]['host'] = 'IMDB_DB';
 $cfg['Servers'][$i]['port'] = '3306';
-$cfg['Servers'][$i]['user'] = 'admin_de_pear';
+$cfg['Servers'][$i]['user'] = 'imdb_admin';
 $cfg['Servers'][$i]['password'] = '{{SECRET_DB_PASSWORD}}';
 $cfg['Servers'][$i]['ssl'] = false; //no ssl; only available within internal docker network
 $cfg['Servers'][$i]['ssl_verify'] = false;
